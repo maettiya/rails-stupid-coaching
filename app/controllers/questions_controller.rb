47771e9a792309966cab41972464705f4c3fa383
Matt McLuckie <mcluckiematt@gmail.com>
2025-11-10 12:23:29 +0900

    Coach Completed

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,19 @@ class QuestionsController < ApplicationController
   def ask
   end
 
+  def answer
+    @question = params[:question]
+    @reply = coach_answer(@question)
+  end
+
+  def coach_answer(your_message)
+    if your_message.include?("?")
+      return "Silly question, get dressed and go to work!"
+    elsif your_message.include?("I am going to work right now!")
+      return ""
+    else
+      return "I don't care, get dressed and go to work!"
+    end
+  end
+
 end
